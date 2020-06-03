c66c6ecafaaad87c4c84f5cd7e7c2e6c4ea7ed04
Sasha Lukas <sashamati@gmail.com>
2020-06-03 14:39:39 -0700

    finished

--- twitteruser/admin.py
+++ twitteruser/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import CustomUser
+from twitteruser.models import CustomUserAdmin
 
-# Register your models here.
+
+admin.site.register(CustomUser, CustomUserAdmin)
