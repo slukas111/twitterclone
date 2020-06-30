c043f7b75c1823133be0ad88220c75780337c56b
Sasha Lukas <sashamati@gmail.com>
2020-06-30 13:56:01 -0700

    finished the notification model. mirate ok

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from twitteruser.models import CustomUser
+from tweet.models import Tweet
 
-# Create your models here.
+
+class Notification(models.Model):
+    contact_user = models.ForeignKey(CustomUser, related_name="contact_user", on_delete=models.CASCADE)
+    notification = models.BooleanField(default=False)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tweet
